83514eb8922e3e986e5f624f303a7ae58de884a0
Kyle-Chelliah <80268385+Kyle-Chelliah@users.noreply.github.com>
2021-08-16 19:29:08 +0200

    Update google_drive.py

--- fastLink-notebook/Fastlink package/1. Input/google_drive.py
+++ fastLink-notebook/Fastlink package/1. Input/google_drive.py
@@ -2,6 +2,7 @@ from ipywidgets import Text
 import pandas as pd
 import os
 from rpy2.robjects import globalenv
+from rpy2.robjects.vectors import StrVector
 import rpy2.robjects as r_objects
 r = r_objects.r
 
