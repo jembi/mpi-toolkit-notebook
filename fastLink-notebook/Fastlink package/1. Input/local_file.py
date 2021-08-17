2d9b0e3e23ce4320fbc5a0be5edba31cb14dabc4
Kyle-Chelliah <80268385+Kyle-Chelliah@users.noreply.github.com>
2021-08-17 09:38:57 +0200

    Update local_file.py

--- fastLink-notebook/Fastlink package/1. Input/local_file.py
+++ fastLink-notebook/Fastlink package/1. Input/local_file.py
@@ -24,29 +24,3 @@ def local_file(uploaded):
     file_list = list(uploaded.keys())
     file = file_list[0]
     return file
-
-# 2.3) Upload and read data
-def read_dataset_other(file):
-
-    file_flag = 0
-    try:
-        globalenv['csv'] = r['read.csv'](file, header=True, stringsAsFactors=False)
-        col_names_r = r('colnames(csv)')
-        col_names = list(col_names_r)
-        r('csv[csv==""] <- NA')
-        r('dfA <- csv[str_detect(csv$ID, "-aaa-"), ]')
-        r('dfB <- csv[str_detect(csv$ID, "-bbb-"), ]')
-        s = r('structure(list(csv = csv, dfA = dfA, dfB = dfB))')
-
-        r('write.csv(csv, file="file.csv")')
-        file = pd.read_csv('file.csv')
-        try:
-            os.remove('file.csv')
-        except OSError:
-            pass
-        file_flag = 1
-    except: # Add specific exception error
-        message = "Cannot find such file"
-        return file_flag, message
-
-    return file_flag, file, col_names, s
