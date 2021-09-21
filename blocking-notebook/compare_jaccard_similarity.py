b4574d6ee52243885919158d517f2e4447284cd4
Bruce Dickie <bruce.dickie@jembi.org>
2021-09-21 16:28:06 +0200

    MPIT-132 > Simplifying Jaccard Similarity

--- blocking-notebook/compare_jaccard_similarity.py
+++ blocking-notebook/compare_jaccard_similarity.py
@@ -5,9 +5,7 @@ def compare(str1, str2, level=0.85):
 
     str1, str2 = str1.lower(), str2.lower()
 
-    if str1 and str2:
-        return jaccard.similarity(str1, str2) >= level
-    return False
+    return jaccard.similarity(str1, str2) >= level
 
 
 def output(name1, name2):
