757fccc2256085b09bb07e2505a3e038cb2887da
Bruce Dickie <bruce.dickie@jembi.org>
2021-09-21 16:24:08 +0200

    MPIT-132 > simplifying damerauLevenshtein

--- blocking-notebook/compare_damerau_levenshtein.py
+++ blocking-notebook/compare_damerau_levenshtein.py
@@ -2,12 +2,8 @@ from fastDamerauLevenshtein import damerauLevenshtein
 
 
 def compare(str1, str2, level=2):
-
-    if str1.lower() == str2.lower():                # in a pure Python implementation, this would be faster
-        return True
-    if str1 and str2:
-        return damerauLevenshtein(str1, str2, False) <= level
-    return False
+    
+    return damerauLevenshtein(str1, str2, False) <= level
 
 
 def output(name1, name2):
